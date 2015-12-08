da5c557417b227dc060c561b9c897c2623a3dd17
Dominik Hofer <me@dominikhofer.com>
2015-12-08 17:18:55 +0100

    Set scalesPageToFit to YES to support non-responsive html.

--- InAppSettingsKit/Controllers/IASKAppSettingsWebViewController.m
+++ InAppSettingsKit/Controllers/IASKAppSettingsWebViewController.m
@@ -40,7 +40,7 @@
     self.webView = [[UIWebView alloc] init];
     self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     self.webView.delegate = self;
-    self.webView.scalesPageToFit = true;
+    self.webView.scalesPageToFit = YES;
     
     self.view = self.webView;
 }

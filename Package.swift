a71ae231e92669d959301741d49619e1c2a00638
Dominik Hofer <me@dominikhofer.com>
2023-02-13 10:44:00 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "InAppSettingsKit",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "InAppSettingsKit",
+            targets: ["InAppSettingsKit"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "InAppSettingsKit"
+        ),
+        .testTarget(
+            name: "InAppSettingsKitTests",
+            dependencies: [
+                "InAppSettingsKit"
+            ],
+            resources: [
+                .copy("Settings.bundle")
+            ]
+        ),
+
+    ]
+)
